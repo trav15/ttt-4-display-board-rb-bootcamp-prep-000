47a59ed5a0e7adf19687d59d72d37a59ffeba70b
Travis Tokuyama <travistokuyama@gmail.com>
2018-08-24 00:58:53 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board
+  one_space = "   "
+  horizontal_line = "-----------"
+  pipe_line = "|"
+  one_row = one_space + pipe_line + one_space + pipe_line + one_space
+  puts one_row
+  puts horizontal_line
+  puts one_row
+  puts horizontal_line
+  puts one_row
+end
