9f0fc4d23c8092b9ec4462e78784e5a9714fd3a2
Travis Tokuyama <travistokuyama@gmail.com>
2018-08-24 01:02:40 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,10 +2,10 @@
 # out the current state.
 
 def display_board (board)
-  one_space = "   "
+  one_space = " "
   horizontal_line = "-----------"
   pipe_line = "|"
-  one_row = one_space + pipe_line + one_space + pipe_line + one_space
+  one_row = one_space + "#{board[0]}" one_space + pipe_line + one_space + pipe_line + one_space
   puts one_row
   puts horizontal_line
   puts one_row
