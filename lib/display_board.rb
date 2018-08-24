f159edab722d933a3ed3293c80ca46324ade394b
Travis Tokuyama <travistokuyama@gmail.com>
2018-08-24 01:04:42 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,10 +8,10 @@ def display_board (board)
   first_row = one_space + "#{board[0]}" one_space + pipe_line + one_space + "#{board[1]}" one_space + pipe_line + one_space + "#{board[2]}" one_space
   second_row = one_space + "#{board[3]}" one_space + pipe_line + one_space + "#{board[4]}" one_space + pipe_line + one_space + "#{board[5]}" one_space
   third_row = one_space + "#{board[6]}" one_space + pipe_line + one_space + "#{board[7]}" one_space + pipe_line + one_space + "#{board[8]}" one_space
-  puts one_row
+  puts first_row
   puts horizontal_line
-  puts one_row
+  puts second_row
   puts horizontal_line
-  puts one_row
+  puts third_row
 end
 
